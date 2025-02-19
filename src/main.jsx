712723dcb357adb7ac1b38ba803068235941ada6
domingohernani <dev.hernani.domingo@gmail.com>
2025-02-19 08:42:14 +0800

    css file structure

--- src/main.jsx
+++ src/main.jsx
@@ -1,10 +1,20 @@
-import { StrictMode } from 'react'
-import { createRoot } from 'react-dom/client'
-import './index.css'
-import App from './App.jsx'
+import { StrictMode } from "react";
+import { createRoot } from "react-dom/client";
 
-createRoot(document.getElementById('root')).render(
+// base folder contains the base styles
+import "./css/base/theme.css";
+import "./css/base/typography.css";
+
+// responsive folder contains the responsive styles
+import "./css/responsive/responsive.css";
+
+// animations folder contains the animations styles
+import "./css/animation/animation.css";
+
+import App from "./App.jsx";
+
+createRoot(document.getElementById("root")).render(
   <StrictMode>
     <App />
-  </StrictMode>,
-)
+  </StrictMode>
+);
