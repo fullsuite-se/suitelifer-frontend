1389bed0edaab556adfca6b951a57dff3a63a056
dan-galano <mooncoreee25@gmail.com>
2025-02-20 15:30:55 +0800

    added nav on "About us" page for mobile and tablet view

--- src/pages/guest/AboutUs.jsx
+++ src/pages/guest/AboutUs.jsx
@@ -1,9 +1,19 @@
 import React from "react";
 import Footer from "../../components/Footer";
+import MobileNav from "../../components/MobileNav";
+import TabletNav from "../../components/TabletNav";
 
 const AboutUs = () => {
   return (
     <section className="">
+      {/* MOBILE NAV */}
+      <div className="sm:hidden">
+        <MobileNav />
+      </div>
+      {/* TABLET NAV */}
+      <div className="tablet-nav">
+        <TabletNav />
+      </div>
       <main className="bg-red-30 h-dvh">
         <p>About Us</p>
       </main>
