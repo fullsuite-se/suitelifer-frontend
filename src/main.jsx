30155d38ef31dd77081a1d59baa67c1919f9b087
domingohernani <dev.hernani.domingo@gmail.com>
2025-02-24 18:24:36 +0800

    about us first section

--- src/main.jsx
+++ src/main.jsx
@@ -8,6 +8,7 @@ import "./css/base/typography.css";
 // responsive folder contains the responsive styles
 import "./css/responsive/footer.css";
 import "./css/responsive/home.css";
+import "./css/responsive/about.css";
 
 // animations folder contains the animations styles
 import "./css/animation/animation.css";
