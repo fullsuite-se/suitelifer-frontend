250ffd60d0044bf6862d9cfe5db338a4607025ff
dan-galano <mooncoreee25@gmail.com>
2025-02-24 19:05:31 +0800

    Refactor HeroSection component and update imports in Home page; adjust styles in MobileNav and TabletNav

--- src/pages/guest/Home.jsx
+++ src/pages/guest/Home.jsx
@@ -1,9 +1,11 @@
 import React from "react";
 import fs_tagline from "../../assets/logos/logo-fs-tagline.svg";
 import banner_img from "../../assets/images/banner-img.svg";
-import HeroSection from "../../components/HeroSection";
+import HeroSection from "../../components/HomeHeroSection";
 import MobileNav from "../../components/MobileNav";
 import TabletNav from "../../components/TabletNav";
+import DesktopNav from "../../components/DesktopNav";
+import Footer from "../../components/Footer";
 
 const Home = () => {
   return (
@@ -16,36 +18,44 @@ const Home = () => {
       <div className="tablet-nav">
         <TabletNav />
       </div>
-      {/* HERO SECTION */}
-      <HeroSection />
-      {/* BANNER SECTION */}
-      <section className="flex pt-5">
-        <div className="banner ml-[9%] flex items-center">
-          <div>
-            <p className="text-primary text-lg md:text-4xl lg:text-7xl">
-              Welcome to
-            </p>
-            <div className="pb-5">
-              <img
-                className="w-full h-full object-contain"
-                src={fs_tagline}
-                alt="Fullsuite tagline"
-              />
+      {/* DESKTOP NAV */}
+      <div className="desktop-nav">
+        <DesktopNav />
+      </div>
+      <div className="lg:flex lg:flex-row-reverse">
+        {/* HERO SECTION */}
+        <HeroSection/>
+        {/* BANNER SECTION */}
+        <section className="flex pt-5 lg:w-2/5 ">
+          <div className="banner ml-[9%] flex items-center">
+            <div>
+              <p className="text-primary text-lg md:text-3xl lg:text-3xl! ">
+                Welcome to
+              </p>
+              <div className="pb-5">
+                <img
+                  className="w-full h-full object-contain"
+                  src={fs_tagline}
+                  alt="Fullsuite tagline"
+                />
+              </div>
+              <button className="btn-primary text-sm sm:text-xl md:text-2xl">
+                Learn more
+              </button>
             </div>
-            <button className="btn-primary text-sm sm:text-xl md:text-2xl">
-              Learn more
-            </button>
           </div>
-        </div>
-        {/* Column 2 */}
-        <div className="flex justify-end items-start ml-5 pb-3">
-          <img
-            className="opacity-70 -z-10 rounded-l-4xl object-cover h-full w-full"
-            src={banner_img}
-            alt="Banner image"
-          />
-        </div>
-      </section>
+          {/* Column 2 */}
+          <div className="flex justify-end items-start ml-5 pb-3 lg:hidden">
+            <img
+              className="opacity-70 -z-10 rounded-l-4xl object-cover h-full w-full"
+              src={banner_img}
+              alt="Banner image"
+            />
+          </div>
+        </section>
+      </div>
+      <div className="h-svw ani"></div>
+      <Footer />
     </>
   );
 };
