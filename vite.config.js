8f883dcdab2f5c78ceaa175f29da0aef6514fee5
domingohernani <dev.hernani.domingo@gmail.com>
2025-02-28 13:50:19 +0800

    Add @material-tailwind/react dependency

--- vite.config.js
+++ vite.config.js
@@ -1,8 +1,17 @@
 import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react";
 import tailwindcss from "@tailwindcss/vite";
+const withMT = require("@material-tailwind/react/utils/withMT");
 
 // https://vite.dev/config/
-export default defineConfig({
+// export default defineConfig({
+//   plugins: [react(), tailwindcss()],
+// });
+
+module.exports = withMT({
+  content: ["./index.html", "./src/**/*.{vue,js,ts,jsx,tsx}"],
+  theme: {
+    extend: {},
+  },
   plugins: [react(), tailwindcss()],
 });
