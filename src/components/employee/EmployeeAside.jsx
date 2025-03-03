c95d856eb04307bd8402857dc03433bfdac4c686
domingohernani <dev.hernani.domingo@gmail.com>
2025-03-03 10:49:12 +0800

    feat: Add Calendar component to EmployeeAside and adjust layout styles

--- src/components/employee/EmployeeAside.jsx
+++ src/components/employee/EmployeeAside.jsx
@@ -1,9 +1,11 @@
 import React from "react";
+import Calendar from "../Calendar";
 
 const EmployeeAside = () => {
   return (
     <aside className="w-52 md:w-64 lg:w-72 h-dvh flex flex-col p-2 xl:p-3">
       <h2 className="font-avenir-black">Events</h2>
+      <Calendar />
     </aside>
   );
 };
