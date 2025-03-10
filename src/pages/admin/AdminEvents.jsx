4ea26f11c0463371db6bdb4cddf31cf62b5fb122
Melbraei Santiago <melbraeisantiago@gmail.com>
2025-03-10 13:29:45 +0800

    Event Calendar

--- src/pages/admin/AdminEvents.jsx
+++ src/pages/admin/AdminEvents.jsx
@@ -1,6 +1,4 @@
-import React, { useState, useContext } from "react";
-import { Calendar, momentLocalizer, Views } from "react-big-calendar";
-import moment from "moment";
+import React, { useState, useContext, useEffect } from "react";
 import {
   Modal,
   TextField,
@@ -11,111 +9,48 @@ import {
   ListItemText,
 } from "@mui/material";
 import logofsfull from "../../assets/logos/logo-fs-full.svg";
-import "react-big-calendar/lib/css/react-big-calendar.css";
 import { SidebarContext } from "./AdminLayout";
 import AppsIcon from "@mui/icons-material/Apps";
-
-const localizer = momentLocalizer(moment);
+import {
+  Dialog,
+  DialogTitle,
+  DialogContent,
+  DialogActions,
+} from "@mui/material";
+import EventCalendar from "./../../components/admin/EventCalendar";
+import moment from "moment";
 
 const AdminEvents = () => {
   const { isOpen, setIsOpen } = useContext(SidebarContext);
-
+  const [openDialog, setOpenDialog] = useState(false);
   const [events, setEvents] = useState([]);
+  const [upcomingEvents, setUpcomingEvents] = useState([]);
   const [isAddModalOpen, setIsAddModalOpen] = useState(false);
-  const [isEventDetailsModalOpen, setIsEventDetailsModalOpen] = useState(false);
-  const [isUpcomingEventModalOpen, setIsUpcomingEventModalOpen] =
-    useState(false);
   const [newEvent, setNewEvent] = useState({
     title: "",
     start: new Date(),
     end: new Date(new Date().setDate(new Date().getDate() + 1)),
     description: "",
   });
-  const [selectedEvent, setSelectedEvent] = useState(null);
-  const [editableEvent, setEditableEvent] = useState(null);
-  const [selectedUpcomingEvent, setSelectedUpcomingEvent] = useState(null);
-  const [view, setView] = useState(Views.MONTH);
-  const [date, setDate] = useState(new Date());
 
-  const handleSelectSlot = ({ start, end }) => {
+  // Update upcoming events when events change
+  useEffect(() => {
+    setUpcomingEvents(
+      events.filter((event) => new Date(event.start) >= new Date())
+    );
+  }, [events]);
+
+  const handleSelectSlot = ({ start }) => {
+    if (
+      new Date(start).setHours(0, 0, 0, 0) < new Date().setHours(0, 0, 0, 0)
+    ) {
+      setOpenDialog(true);
+      return;
+    }
     setNewEvent({
       ...newEvent,
       start,
-      end,
-    });
-    setIsAddModalOpen(true);
-  };
-  const CustomToolbar = (toolbar) => {
-    return (
-      <div className="rbc-toolbar flex w-full">
-        <div className="flex w-150 justify-start">
-          <div className="flex w-120 justify-between">
-            <button
-              onClick={() => toolbar.onNavigate("PREV")}
-              style={{ color: "#000000", fontWeight: "bold" }}
-            >
-              Back
-            </button>
-            <span
-              style={{
-                fontFamily: "Avenir-Black, sans-serif",
-                fontSize: "24px",
-                fontWeight: "bold",
-                color: "#000000",
-              }}
-            >
-              {moment(toolbar.date).format("MMMM YYYY")}
-            </span>
-            <button
-              onClick={() => toolbar.onNavigate("NEXT")}
-              style={{ color: "#000000", fontWeight: "bold" }}
-            >
-              Next
-            </button>
-          </div>
-        </div>
-
-        <span className="">
-          <button
-            onClick={() => toolbar.onNavigate("TODAY")}
-            style={{ color: "#000000", fontWeight: "bold" }}
-          >
-            Today
-          </button>
-          <button
-            onClick={() => toolbar.onView("month")}
-            style={{ color: "#000000", fontWeight: "bold" }}
-          >
-            Month
-          </button>
-          <button
-            onClick={() => toolbar.onView("week")}
-            style={{ color: "#000000", fontWeight: "bold" }}
-          >
-            Week
-          </button>
-          <button
-            onClick={() => toolbar.onView("day")}
-            style={{ color: "#000000", fontWeight: "bold" }}
-          >
-            Day
-          </button>
-          <button
-            onClick={() => toolbar.onView("agenda")}
-            style={{ color: "#000000", fontWeight: "bold" }}
-          >
-            Agenda
-          </button>
-        </span>
-      </div>
-    );
-  };
-  const handleAddEventButtonClick = () => {
-    setNewEvent({
-      title: "",
-      start: new Date(),
-      end: new Date(new Date().setDate(new Date().getDate() + 1)),
-      description: "",
+      end: new Date(start.getTime() + 60 * 60 * 1000),
     });
     setIsAddModalOpen(true);
   };
@@ -123,6 +58,8 @@ const AdminEvents = () => {
   const handleAddEvent = () => {
     setEvents([...events, newEvent]);
     setIsAddModalOpen(false);
+
+    // Reset newEvent state
     setNewEvent({
       title: "",
       start: new Date(),
@@ -131,109 +68,70 @@ const AdminEvents = () => {
     });
   };
 
-  const handleSelectEvent = (event) => {
-    setSelectedEvent(event);
-    setEditableEvent(event);
-    setIsEventDetailsModalOpen(true);
-  };
-
-  const handleEditEvent = () => {
-    setEvents(
-      events.map((event) => (event === selectedEvent ? editableEvent : event))
-    );
-    setIsEventDetailsModalOpen(false);
-    setSelectedEvent(null);
-    setEditableEvent(null);
-  };
-
-  const handleNavigate = (action) => {
-    const newDate = Calendar.momentLocalizer(moment).navigate(date, action);
-    setDate(newDate);
-  };
-
-  const handleUpcomingEventClick = (event) => {
-    setSelectedUpcomingEvent(event);
-    setIsUpcomingEventModalOpen(true);
-  };
-
   return (
     <div className="bg-white p-2">
-      <header className="container flex h-16 items-center justify-between">
+      <header className="flex justify-between items-center h-16">
         <button className="sm:hidden" onClick={() => setIsOpen(!isOpen)}>
           <AppsIcon sx={{ fontSize: "48px" }} />
         </button>
         <img src={logofsfull} alt="Fullsuite Logo" className="h-8" />
-
-        <div className="flex gap-2">
-          <button className="btn-primary" onClick={handleAddEventButtonClick}>
-            <span className="mr-2">+</span> EVENT
-          </button>
-        </div>
+        <button className="btn-primary" onClick={() => setIsAddModalOpen(true)}>
+          + EVENT
+        </button>
       </header>
-      <div className="p-4 items-center justify-center w-full flex">
-        <Calendar
-          localizer={localizer}
-          events={events}
-          startAccessor="start"
-          endAccessor="end"
-          selectable
-          onSelectSlot={handleSelectSlot}
-          onSelectEvent={handleSelectEvent}
-          style={{ height: 700, width: 1000 }}
-          dayPropGetter={(date) => {
-            const day = date.getDay();
-            if (day === 0 || day === 6) {
-              return {
-                style: {
-                  backgroundColor: "#ffcccc",
-                  fontStyle: "italic",
-                },
-              };
-            }
-            return {};
-          }}
-          components={{
-            toolbar: CustomToolbar,
-          }}
-          view={view}
-          onView={(newView) => setView(newView)}
-          date={date}
-          onNavigate={(newDate) => setDate(newDate)}
-        />
 
-        <div className="p-4 ">
-          <Typography variant="h5">Upcoming Events</Typography>
-          {events.length === 0 ? (
-            <Typography variant="body1" color="textSecondary" fontSize={"36px"}>
-              No Upcoming Events
-            </Typography>
-          ) : (
-            <List>
-              {events
-                .sort((a, b) => new Date(a.start) - new Date(b.start))
-                .slice(0)
-                .map((event, index) => (
-                  <ListItem
-                    button
-                    key={index}
-                    onClick={() => handleUpcomingEventClick(event)}
-                  >
-                    <ListItemText
-                      primary={
-                        event.title.length > 30
-                          ? `${event.title.slice(0, 30)}...`
-                          : event.title
-                      }
-                      secondary={`${moment(event.start).format(
-                        "MMMM Do YYYY, h:mm a"
-                      )} - ${moment(event.end).format("MMMM Do YYYY, h:mm a")}`}
-                    />
-                  </ListItem>
-                ))}
-            </List>
-          )}
+      <TextField label="Search Events" fullWidth margin="normal" />
+
+      <div className="flex gap-8">
+        <div className="bg-white shadow-lg rounded-lg p-4 w-300">
+          <div className="border border-gray-300 rounded-lg overflow-hidden">
+            <EventCalendar
+              events={events}
+              onSelectSlot={handleSelectSlot}
+              onSelectEvent={() => {}}
+              onAddEvent={() => setIsAddModalOpen(true)}
+            />
+          </div>
+        </div>
+
+        {/* Upcoming Events Section */}
+        <div className="mt-6">
+          <Typography variant="h4">Upcoming Events</Typography>
+          <List>
+            {upcomingEvents
+              .sort((a, b) => new Date(a.start) - new Date(b.start))
+              .slice(0, 5) 
+              .map((event, index) => (
+                <ListItem key={index}>
+                  <ListItemText
+                    primary={event.title}
+                    secondary={moment(event.start).format(
+                      "MMMM Do YYYY, h:mm a"
+                    )}
+                  />
+                </ListItem>
+              ))}
+            {upcomingEvents.length === 0 && (
+              <Typography>No upcoming events.</Typography>
+            )}
+          </List>
         </div>
       </div>
+
+      {/* Invalid Date Selection Dialog */}
+      <Dialog open={openDialog} onClose={() => setOpenDialog(false)}>
+        <DialogTitle>Invalid Date Selection</DialogTitle>
+        <DialogContent>
+          You can only add events from today onwards.
+        </DialogContent>
+        <DialogActions>
+          <button onClick={() => setOpenDialog(false)} className="btn-primary">
+            OK
+          </button>
+        </DialogActions>
+      </Dialog>
+
+      {/* Add Event Modal */}
       <Modal open={isAddModalOpen} onClose={() => setIsAddModalOpen(false)}>
         <Box
           sx={{
@@ -241,25 +139,13 @@ const AdminEvents = () => {
             top: "50%",
             left: "50%",
             transform: "translate(-50%, -50%)",
-            bgcolor: "#ffffff",
             p: 4,
+            bgcolor: "white",
             borderRadius: 1,
             boxShadow: 24,
           }}
         >
-          <Typography
-            variant="h6"
-            mb={2}
-            sx={{
-              fontFamily: "avenir-Black",
-              fontSize: "28px",
-              justifyItems: "center",
-              width: "full",
-              display: "flex",
-              justifyContent: "center",
-              color: "#0097b2",
-            }}
-          >
+          <Typography variant="h6" mb={2} className="text-center text-blue-600">
             Add New Event
           </Typography>
           <TextField
@@ -311,145 +197,6 @@ const AdminEvents = () => {
               Add Event
             </button>
           </Box>
-        </Box>
-      </Modal>
-      <Modal
-        open={isEventDetailsModalOpen}
-        onClose={() => setIsEventDetailsModalOpen(false)}
-      >
-        <Box
-          sx={{
-            position: "absolute",
-            top: "50%",
-            left: "50%",
-            transform: "translate(-50%, -50%)",
-            bgcolor: "background.paper",
-            p: 4,
-            borderRadius: 1,
-            boxShadow: 24,
-          }}
-        >
-          {editableEvent && (
-            <>
-              <Typography variant="h6" mb={2}>
-                Edit Event
-              </Typography>
-              <TextField
-                fullWidth
-                label="Title"
-                value={editableEvent.title}
-                onChange={(e) =>
-                  setEditableEvent({ ...editableEvent, title: e.target.value })
-                }
-                margin="normal"
-              />
-              <TextField
-                fullWidth
-                label="Start Date"
-                type="datetime-local"
-                value={moment(editableEvent.start).format("YYYY-MM-DDTHH:mm")}
-                onChange={(e) =>
-                  setEditableEvent({
-                    ...editableEvent,
-                    start: new Date(e.target.value),
-                  })
-                }
-                margin="normal"
-              />
-              <TextField
-                fullWidth
-                label="End Date"
-                type="datetime-local"
-                value={moment(editableEvent.end).format("YYYY-MM-DDTHH:mm")}
-                onChange={(e) =>
-                  setEditableEvent({
-                    ...editableEvent,
-                    end: new Date(e.target.value),
-                  })
-                }
-                margin="normal"
-              />
-              <TextField
-                fullWidth
-                label="Description"
-                value={editableEvent.description}
-                onChange={(e) =>
-                  setEditableEvent({
-                    ...editableEvent,
-                    description: e.target.value,
-                  })
-                }
-                margin="normal"
-              />
-              <Box sx={{ display: "flex", justifyContent: "flex-end" }}>
-                <button
-                  onClick={() => setIsEventDetailsModalOpen(false)}
-                  className="btn-light"
-                >
-                  Cancel
-                </button>
-                <button onClick={handleEditEvent} className="btn-primary">
-                  Save Changes
-                </button>
-              </Box>
-            </>
-          )}
-        </Box>
-      </Modal>
-      <Modal
-        open={isUpcomingEventModalOpen}
-        onClose={() => setIsUpcomingEventModalOpen(false)}
-      >
-        <Box
-          sx={{
-            position: "absolute",
-            top: "50%",
-            left: "50%",
-            transform: "translate(-50%, -50%)",
-            bgcolor: "#ffffff",
-            p: 4,
-            borderRadius: 1,
-            boxShadow: 24,
-          }}
-        >
-          {selectedUpcomingEvent && (
-            <>
-              <Typography
-                variant="h5"
-                mb={2}
-                sx={{ color: "#0097b2", fontFamily: "avenir-Black" }}
-              >
-                Upcoming Event Details
-              </Typography>
-              <Typography variant="body1" mb={2}>
-                <strong>Title:</strong> {selectedUpcomingEvent.title}
-              </Typography>
-              <Typography variant="body1" mb={2}>
-                <strong>Start:</strong>{" "}
-                {moment(selectedUpcomingEvent.start).format(
-                  "MMMM Do YYYY, h:mm a"
-                )}
-              </Typography>
-              <Typography variant="body1" mb={2}>
-                <strong>End:</strong>{" "}
-                {moment(selectedUpcomingEvent.end).format(
-                  "MMMM Do YYYY, h:mm a"
-                )}
-              </Typography>
-              <Typography variant="body1" mb={2}>
-                <strong>Description:</strong>{" "}
-                {selectedUpcomingEvent.description}
-              </Typography>
-              <Box sx={{ display: "flex", justifyContent: "flex-end" }}>
-                <button
-                  onClick={() => setIsUpcomingEventModalOpen(false)}
-                  className="btn-primary"
-                >
-                  Close
-                </button>
-              </Box>
-            </>
-          )}
         </Box>
       </Modal>
     </div>
