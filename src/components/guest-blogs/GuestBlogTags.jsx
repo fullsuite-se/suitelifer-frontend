cdbedbed527fcd2d36fa50003272da8939ff0dc9
ljsalcedo-dev <jau.salcedo04@gmail.com>
2025-03-20 15:17:53 +0800

    feat: update GuestBlogTags component to use dynamic tags from props

--- src/components/guest-blogs/GuestBlogTags.jsx
+++ src/components/guest-blogs/GuestBlogTags.jsx
@@ -1,28 +1,35 @@
-import { useState } from "react";
+import { useEffect, useState } from "react";
+import api from "../../utils/axios";
+import companyBlogs from "../home/CompanyBlogsList";
+import TwoCirclesLoader from "../../assets/loaders/TwoCirclesLoader";
 
-const categories = ["All", "Business", "Employees", "Startups", "Finance", "Baguio"];
+const categories = [
+  "All",
+  "Business",
+  "Employees",
+  "Startups",
+  "Finance",
+  "Baguio",
+];
 
-const GuestBlogTags = () => {
-  const [activeTag, setActiveTag] = useState("All");
-
+const GuestBlogTags = ({ companyBlogTags, activeTag, handleTagClick }) => {
   return (
     <div className="w-full overflow-x-auto">
       <div className="flex gap-2 sm:gap-3 md:gap-4 align-middle justify-start px-2 sm:px-3 md:px-4 whitespace-nowrap md:justify-center">
-        {categories.map((category) => (
+        {companyBlogTags.map((cblogTag, index) => (
           <button
-            key={category}
-            onClick={() => setActiveTag(category)}
+            key={index}
+            onClick={() => handleTagClick(cblogTag.tagId)}
             className={`px-4 py-1 sm:px-5 md:px-6 text-sm sm:text-base md:text-lg rounded-full border-1 border-primary text-primary transition-all duration-300 
-        ${
-          activeTag === category
-            ? "bg-primary text-white"
-            : "hover:bg-primary hover:text-white"
-        }`}
+      ${
+        activeTag === cblogTag.tagId
+          ? "bg-primary text-white"
+          : "hover:bg-primary hover:text-white"
+      }`}
           >
-            {category}
+            {cblogTag.tagName}
           </button>
         ))}
-      <div className="pr-2"></div>
       </div>
     </div>
   );
