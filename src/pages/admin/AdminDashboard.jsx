763733659dfaa32d691e75e42d0eea542f8e6e40
domingohernani <dev.hernani.domingo@gmail.com>
2025-03-24 13:42:59 +0800

    refactor: remove unused header elements from AdminDashboard

--- src/pages/admin/AdminDashboard.jsx
+++ src/pages/admin/AdminDashboard.jsx
@@ -7,7 +7,6 @@ import {
   YAxis,
   Tooltip,
 } from "recharts";
-import logofsfull from "../../assets/logos/logo-fs-full.svg";
 import api from "../../utils/axios";
 import atsAPI from "../../utils/atsAPI";
 
@@ -125,57 +124,6 @@ const AdminDashboard = () => {
   return (
     <div className="max-h-100vh">
       {/* Header */}
-      <header className="container flex h-12 items-center justify-end flex-wrap">
-        {/* <div className="hidden lg:flex md-flex gap-4 items-center ">
-          <img src={logofsfull} alt="Fullsuite Logo" className="h-8" />
-        </div> */}
-
-        <div className="flex gap-3">
-          {/* Button for desktop */}
-          <button className="text-gray-500 hidden sm:block border border-gray-200 bg-gray-100 px-3 py-2 rounded-md cursor-pointer">
-            + Job Listing
-          </button>
-          <button className="text-gray-500 hidden sm:block border border-gray-200 bg-gray-100 px-3 py-2 rounded-md cursor-pointer">
-            + Industry
-          </button>
-
-          {/* Icon Button for Mobile */}
-          <button className="text-gray-500 border rounded-md cursor-pointer bg-gray-100 border-gray-200 sm:hidden px-3 py-2 flex items-center gap-1">
-            <span>+</span>
-            <svg
-              xmlns="http://www.w3.org/2000/svg"
-              fill="none"
-              viewBox="0 0 24 24"
-              strokeWidth={1.5}
-              stroke="currentColor"
-              className="size-5"
-            >
-              <path
-                strokeLinecap="round"
-                strokeLinejoin="round"
-                d="M20.25 14.15v4.25c0 1.094-.787 2.036-1.872 2.18-2.087.277-4.216.42-6.378.42s-4.291-.143-6.378-.42c-1.085-.144-1.872-1.086-1.872-2.18v-4.25m16.5 0a2.18 2.18 0 0 0 .75-1.661V8.706c0-1.081-.768-2.015-1.837-2.175a48.114 48.114 0 0 0-3.413-.387m4.5 8.006c-.194.165-.42.295-.673.38A23.978 23.978 0 0 1 12 15.75c-2.648 0-5.195-.429-7.577-1.22a2.016 2.016 0 0 1-.673-.38m0 0A2.18 2.18 0 0 1 3 12.489V8.706c0-1.081.768-2.015 1.837-2.175a48.111 48.111 0 0 1 3.413-.387m7.5 0V5.25A2.25 2.25 0 0 0 13.5 3h-3a2.25 2.25 0 0 0-2.25 2.25v.894m7.5 0a48.667 48.667 0 0 0-7.5 0M12 12.75h.008v.008H12v-.008Z"
-              />
-            </svg>
-          </button>
-          <button className="text-gray-500 border rounded-md cursor-pointer bg-gray-100 border-gray-200 sm:hidden px-3 py-2 flex items-center gap-1">
-            <span>+</span>{" "}
-            <svg
-              xmlns="http://www.w3.org/2000/svg"
-              fill="none"
-              viewBox="0 0 24 24"
-              strokeWidth={1.5}
-              stroke="currentColor"
-              className="size-5"
-            >
-              <path
-                strokeLinecap="round"
-                strokeLinejoin="round"
-                d="M3.75 21h16.5M4.5 3h15M5.25 3v18m13.5-18v18M9 6.75h1.5m-1.5 3h1.5m-1.5 3h1.5m3-6H15m-1.5 3H15m-1.5 3H15M9 21v-3.375c0-.621.504-1.125 1.125-1.125h3.75c.621 0 1.125.504 1.125 1.125V21"
-              />
-            </svg>
-          </button>
-        </div>
-      </header>
 
       <section className="mt-3 mb-4 grid grid-cols-4 grid-rows-[7rem] [&>*]:bg-gray-100 [&>*]:border [&>*]:border-gray-200 gap-4">
         <div
