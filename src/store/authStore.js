199f78977869e52334c8737abb75ef30c081d251
domingohernani <dev.hernani.domingo@gmail.com>
2025-03-24 15:51:43 +0800

    feat: add searchValue store for managing search input state

--- src/store/authStore.js
+++ src/store/authStore.js
@@ -1,8 +1,10 @@
 import { create } from "zustand";
 import { services } from "./stores/services";
 import { user } from "./stores/user";
+import { searchValue } from "./stores/searchValue";
 
 export const useStore = create((...a) => ({
   ...services(...a),
   ...user(...a),
+  ...searchValue(...a),
 }));
