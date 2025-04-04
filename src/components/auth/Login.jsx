0858a165e25a4b5a44ead5f153eb93d162498bb0
AllenJalvaro <alllenjjames@gmail.com>
2025-04-04 16:02:31 +0800

    feat: add TestCredentials component with copy functionality and fetch FAQs from API

--- src/components/auth/Login.jsx
+++ src/components/auth/Login.jsx
@@ -12,11 +12,121 @@ import {
   GoogleReCaptchaProvider,
   useGoogleReCaptcha,
 } from "react-google-recaptcha-v3";
+import TestCredentials from "./TestCredentials";
 
-const LoginForm = () => {
+// const LoginForm = () => {
+//   const navigate = useNavigate();
+//   const [email, setEmail] = useState("");
+//   const [password, setPassword] = useState("");
+//   const [showPassword, setShowPassword] = useState(false);
+//   const [loading, setLoading] = useState(false);
+//   const { executeRecaptcha } = useGoogleReCaptcha();
+
+//   const handleLogin = async (e) => {
+//     e.preventDefault();
+
+//     if (!executeRecaptcha) {
+//       toast.error("reCAPTCHA is not ready.");
+//       return;
+//     }
+
+//     const recaptchaToken = await executeRecaptcha("login");
+
+//     if (!email || !password) {
+//       toast.error("Please enter both email and password.");
+//       return;
+//     }
+
+//     try {
+//       setLoading(true);
+//       const response = await api.post("/api/login", {
+//         email,
+//         password,
+//         recaptchaToken,
+//       });
+
+//       if (response.data.accessToken) {
+//         toast.success("Welcome back! You have successfully logged in.");
+//         navigate("/app/blogs-feed");
+//       } else if (response.data.recaptchaError) {
+//         toast.success(response.data.message);
+//       } else {
+//         toast.error("Login failed. Please check your credentials.");
+//       }
+//     } catch (error) {
+//       setLoading(false);
+//       if (error.response?.status === 400) {
+//         toast.error("Invalid email or password. Please try again.");
+//         setEmail("");
+//         setPassword("");
+//       } else {
+//         toast.error(
+//           `Error: ${error.response?.data?.message || "Something went wrong."}`
+//         );
+//       }
+//     } finally {
+//       setLoading(false);
+//     }
+//   };
+
+//   return (
+//     <form onSubmit={handleLogin} className="flex flex-col gap-5">
+//       <div>
+//         <input
+//           type="text"
+//           id="email"
+//           value={email}
+//           placeholder="Email"
+//           onChange={(e) => setEmail(e.target.value)}
+//           className="w-full p-3 border-none rounded-md bg-primary/10 focus:outline-none focus:ring-2 focus:ring-primary placeholder-primary/50"
+//         />
+//       </div>
+//       <div className="relative">
+//         <input
+//           type={showPassword ? "text" : "password"}
+//           id="password"
+//           value={password}
+//           placeholder="Password"
+//           onChange={(e) => setPassword(e.target.value)}
+//           className="w-full p-3 border-none rounded-md bg-primary/10 focus:outline-none focus:ring-2 focus:ring-primary placeholder-primary/50"
+//         />
+//         <button
+//           type="button"
+//           className="absolute right-3 top-4 text-gray-500 hover:text-gray-700"
+//           onClick={() => setShowPassword(!showPassword)}
+//         >
+//           {showPassword ? (
+//             <EyeSlashIcon className="w-5 h-5 cursor-pointer text-primary" />
+//           ) : (
+//             <EyeIcon className="w-5 h-5 cursor-pointer text-primary" />
+//           )}
+//         </button>
+//       </div>
+//       <button
+//         type="submit"
+//         className="mt-5 w-full bg-primary p-3 rounded-xl text-white font-avenir-black cursor-pointer"
+//       >
+//         {loading ? (
+//           <div className="mx-auto w-fit">
+//             <TwoCirclesLoader
+//               bg={"transparent"}
+//               color1={"#bfd1a0"}
+//               color2={"#ffffff"}
+//               width={"135"}
+//               height={"24"}
+//             />
+//           </div>
+//         ) : (
+//           "LOG IN"
+//         )}
+//       </button>
+//     </form>
+//   );
+// };
+
+//with test credentials
+const LoginForm = ({ email, password, setEmail, setPassword }) => {
   const navigate = useNavigate();
-  const [email, setEmail] = useState("");
-  const [password, setPassword] = useState("");
   const [showPassword, setShowPassword] = useState(false);
   const [loading, setLoading] = useState(false);
   const { executeRecaptcha } = useGoogleReCaptcha();
@@ -74,9 +184,9 @@ const LoginForm = () => {
         <input
           type="text"
           id="email"
-          value={email}
+          value={email} 
           placeholder="Email"
-          onChange={(e) => setEmail(e.target.value)}
+          onChange={(e) => setEmail(e.target.value)} 
           className="w-full p-3 border-none rounded-md bg-primary/10 focus:outline-none focus:ring-2 focus:ring-primary placeholder-primary/50"
         />
       </div>
@@ -84,28 +194,17 @@ const LoginForm = () => {
         <input
           type={showPassword ? "text" : "password"}
           id="password"
-          value={password}
+          value={password} 
           placeholder="Password"
-          onChange={(e) => setPassword(e.target.value)}
+          onChange={(e) => setPassword(e.target.value)} 
           className="w-full p-3 border-none rounded-md bg-primary/10 focus:outline-none focus:ring-2 focus:ring-primary placeholder-primary/50"
         />
-        <button
-          type="button"
-          className="absolute right-3 top-4 text-gray-500 hover:text-gray-700"
-          onClick={() => setShowPassword(!showPassword)}
-        >
-          {showPassword ? (
-            <EyeSlashIcon className="w-5 h-5 cursor-pointer text-primary" />
-          ) : (
-            <EyeIcon className="w-5 h-5 cursor-pointer text-primary" />
-          )}
-        </button>
       </div>
       <button
         type="submit"
-        className="mt-5 w-full bg-primary p-3 rounded-xl text-white font-avenir-black cursor-pointer"
+        className="mt-5 w-full bg-primary p-3 rounded-xl text-white font-avenir-black"
       >
-        {loading ? (
+       {loading ? (
           <div className="mx-auto w-fit">
             <TwoCirclesLoader
               bg={"transparent"}
@@ -124,9 +223,14 @@ const LoginForm = () => {
 };
 
 // Main Login Component
+
 const Login = () => {
   const navigate = useNavigate();
   const vantaRef = useRef(null);
+
+  const [showCredentials, setShowCredentials] = useState(false);
+  const [email, setEmail] = useState("");
+  const [password, setPassword] = useState("");
 
   useEffect(() => {
     const fetchUser = async () => {
@@ -169,28 +273,50 @@ const Login = () => {
 
   return (
     <GoogleReCaptchaProvider reCaptchaKey={import.meta.env.VITE_RECAPTCHA_SITE}>
-      <div
-        id="vanta-bg"
-        className="w-screen h-screen flex justify-start items-center bg-white"
-      >
-        <div
-          className="bg-white mx-auto rounded-2xl p-10 py-16 border border-gray-200"
-          style={{ width: "min(90%, 600px)" }}
-        >
-          <img
-            src={fullsuite}
-            alt="FullSuite"
-            onClick={() => navigate("/")}
-            className="w-28 h-auto mx-auto cursor-pointer"
-          />
-          <p className="text-center text-base my-4 text-gray-500 mb-10">
-            Welcome to SuiteLifer!
-          </p>
-          <LoginForm /> {/* Wrapped safely inside GoogleReCaptchaProvider */}
+      <div id="vanta-bg" className="min-h-screen bg-white overflow-y-auto">
+        <div className={`flex flex-col items-center ${!showCredentials ? "justify-center min-h-screen" : "pt-20"}`}>
+          <div
+            className="bg-white mx-auto rounded-2xl p-10 py-16 border border-gray-200"
+            style={{ width: "min(90%, 600px)" }}
+          >
+            <img
+              src={fullsuite}
+              alt="FullSuite"
+              onClick={() => navigate("/")}
+              className="w-28 h-auto mx-auto cursor-pointer"
+            />
+            <p className="text-center text-base my-4 text-gray-500 mb-10">
+              Welcome SuiteLifer!
+            </p>
+            <LoginForm
+              email={email}
+              password={password}
+              setEmail={setEmail}
+              setPassword={setPassword}
+            />
+            <div className="flex justify-center">
+              <button
+                className="mt-10 px-2 py-1 text-xss text-primary border border-primary-300 rounded hover:bg-gray-100"
+                onClick={() => setShowCredentials(!showCredentials)}
+              >
+                TEST CREDENTIALS
+              </button>
+            </div>
+          </div>
+  
+          {showCredentials && (
+            <div
+              className="bg-white mx-auto rounded-2xl p-5 mt-10"
+              style={{ width: "min(90%, 600px)" }}
+            >
+              <TestCredentials setEmail={setEmail} setPassword={setPassword} />
+            </div>
+          )}
         </div>
       </div>
     </GoogleReCaptchaProvider>
   );
+  
 };
 
 export default Login;
