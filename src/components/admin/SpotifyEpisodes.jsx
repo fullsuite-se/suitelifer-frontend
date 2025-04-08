9b96adc340bc83b9e128ea196eb391456b650043
ljsalcedo-dev <jau.salcedo04@gmail.com>
2025-04-08 13:55:32 +0800

    Refactor Spotify episode components to support dynamic embed types and improve URL extraction logic

--- src/components/admin/SpotifyEpisodes.jsx
+++ src/components/admin/SpotifyEpisodes.jsx
@@ -13,7 +13,8 @@ import { useStore } from "../../store/authStore";
 import { showConfirmationToast } from "../toasts/confirm";
 
 const extractSpotifyId = (url) => {
-  return url.split("episode/")[1].split("?")[0];
+  const match = url.match(/(?:episode|playlist)\/([^?]+)/);
+  return match ? match[1] : null;
 };
 
 const isValidEpisodeUrl = (url) => {
@@ -169,7 +170,7 @@ const SpotifyEpisodes = () => {
   }, [dataUpdated]);
 
   return (
-    <div className="w-full space-y-4">
+    <div className="w-full space-y-4 bg">
       {error && (
         <div className="fixed inset-0 flex items-center justify-center">
           <div className="bg-white p-6 rounded-2xl shadow-lg max-w-md w-full border-2 border-red-500/95">
@@ -190,75 +191,77 @@ const SpotifyEpisodes = () => {
         </div>
       )}
 
-      <div className="flex gap-2">
-        <input
-          type="text"
-          value={episodeDetails.spotifyId}
-          onChange={(e) =>
-            setEpisodeDetails((ed) => ({ ...ed, spotifyId: e.target.value }))
-          }
-          placeholder="Enter Spotify episode URL"
-          className="border p-2 rounded-2xl w-full"
-        />
-        {episodeDetails.episodeId !== null ? (
-          <>
+      <div className="overflow-y-auto space-y-4 bg-amber-300">
+        <div className="flex gap-2">
+          <input
+            type="text"
+            value={episodeDetails.spotifyId}
+            onChange={(e) =>
+              setEpisodeDetails((ed) => ({ ...ed, spotifyId: e.target.value }))
+            }
+            placeholder="Enter Spotify episode URL"
+            className="border p-2 rounded-2xl w-full"
+          />
+          {episodeDetails.episodeId !== null ? (
+            <>
+              <button
+                onClick={(e) => handleAddEditEpisode(e)}
+                className="text-primary"
+              >
+                <PlusIcon className="size-5" />
+              </button>
+              <button onClick={cancelEdit} className="text-primary">
+                <XCircleIcon className="size-8" />
+              </button>
+            </>
+          ) : (
             <button
               onClick={(e) => handleAddEditEpisode(e)}
-              className="text-primary"
+              className="btn-light"
             >
-              <PlusIcon className="size-5" />
+              <PlusIcon className="size-5 sm:size-7" />
             </button>
-            <button onClick={cancelEdit} className="text-primary">
-              <XCircleIcon className="size-8" />
-            </button>
-          </>
+          )}
+        </div>
+
+        {episodes.length === 0 ? (
+          <div className="p-4 text-center text-gray-600">
+            No episodes added yet.
+          </div>
         ) : (
-          <button
-            onClick={(e) => handleAddEditEpisode(e)}
-            className="btn-light"
-          >
-            <PlusIcon className="size-5 sm:size-7" />
-          </button>
+          episodes.map((episode, index) => {
+            return (
+              <div key={index} className="flex flex-col sm:flex-row gap-2">
+                <div className="w-full sm:w-[96%]">
+                  <SingleSpotifyEmbed
+                    spotifyId={episode.spotifyId}
+                    embedType={episode.embedType}
+                  />
+                </div>
+                <div className="flex w-full sm:w-[4%] sm:min-w-[5%] flex-row sm:flex-col justify-center items-center gap-2">
+                  <button
+                    onClick={() => {
+                      setEpisodeDetails({
+                        episodeId: episode.episodeId,
+                        spotifyId: `https://open.spotify.com/episode/${episode.spotifyId}`,
+                      });
+                    }}
+                    className="bg-primary text-white w-[48%] sm:w-full rounded-2xl cursor-pointer transition-all duration-500 hover:bg-[#007a8e] h-[50%]"
+                  >
+                    <EditIcon className="size-7" />
+                  </button>
+                  <button
+                    onClick={() => handleDeleteClick(episode.episodeId)}
+                    className="bg-primary text-white w-[48%] sm:w-full rounded-2xl cursor-pointer transition-all duration-500 hover:bg-[#007a8e] h-[50%]"
+                  >
+                    <DeleteIcon className="size-7" />
+                  </button>
+                </div>
+              </div>
+            );
+          })
         )}
       </div>
-
-      {episodes.length === 0 ? (
-        <div className="p-4 text-center text-gray-600">
-          No episodes added yet.
-        </div>
-      ) : (
-        episodes.map((episode) => {
-          return (
-            <div
-              key={episode.episodeId}
-              className="flex flex-col sm:flex-row gap-2"
-            >
-              <div className="w-full sm:w-[96%]">
-                <SingleSpotifyEmbed spotifyId={episode.spotifyId} />
-              </div>
-              <div className="flex w-full sm:w-[4%] sm:min-w-[5%] flex-row sm:flex-col justify-center items-center gap-2">
-                <button
-                  onClick={() => {
-                    setEpisodeDetails({
-                      episodeId: episode.episodeId,
-                      spotifyId: `https://open.spotify.com/episode/${episode.spotifyId}`,
-                    });
-                  }}
-                  className="bg-primary text-white w-[48%] sm:w-full rounded-2xl cursor-pointer transition-all duration-500 hover:bg-[#007a8e] h-[50%]"
-                >
-                  <EditIcon className="size-7" />
-                </button>
-                <button
-                  onClick={() => handleDeleteClick(episode.episodeId)}
-                  className="bg-primary text-white w-[48%] sm:w-full rounded-2xl cursor-pointer transition-all duration-500 hover:bg-[#007a8e] h-[50%]"
-                >
-                  <DeleteIcon className="size-7" />
-                </button>
-              </div>
-            </div>
-          );
-        })
-      )}
     </div>
   );
 };
--- src/components/home/SingleSpotifyEmbed.jsx
+++ src/components/home/SingleSpotifyEmbed.jsx
@@ -1,15 +1,17 @@
-const SingleSpotifyEmbed = ({ spotifyId }) => {
-  
-    return (
-      <iframe
-      className="rounded-2xl shadow-md"
-        src={`https://open.spotify.com/embed/episode/${spotifyId}?utm_source=generator`}
-        width="100%"
-        height="152"
-        allow="autoplay; clipboard-write; encrypted-media; fullscreen; picture-in-picture"
-        loading="lazy"
-      ></iframe>
-    );
-  };  
+import React from "react";
 
-  export default SingleSpotifyEmbed;
+const SingleSpotifyEmbed = ({ spotifyId, embedType, index }) => {
+  return (
+    <iframe
+      src={`https://open.spotify.com/embed/${embedType.toLowerCase()}/${spotifyId}?utm_source=generator`}
+      width="100%"
+      height={index === 0 ? "352" : "152"}
+      style={{ borderRadius: "12px" }}
+      allow="autoplay; clipboard-write; encrypted-media; fullscreen; picture-in-picture"
+      loading="lazy"
+      key={index}
+    ></iframe>
+  );
+};
+
+export default SingleSpotifyEmbed;
