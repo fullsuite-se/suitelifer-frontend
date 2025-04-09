d8c91e8ccaae5a3fc35f72bdbc6e1c7d46647431
ljsalcedo-dev <jau.salcedo04@gmail.com>
2025-04-09 10:58:32 +0800

    feat: implement personality test fetching and display in EmployeePersonalityTest component

--- src/pages/employee/EmployeePersonalityTest.jsx
+++ src/pages/employee/EmployeePersonalityTest.jsx
@@ -1,10 +1,39 @@
-import React from "react";
-import PersonalityTest from "../../components/employee/PersonalityTest";
+import React, { useEffect, useState } from "react";
+import TestCardComponent from "../../components/employee/TestCard";
+import api from "../../utils/axios";
 
 const EmployeePersonalityTest = () => {
+  const [personalityTests, setPersonalityTests] = useState([]);
+
+  const fetchPersonalityTests = async () => {
+    try {
+      const response = await api.get("/api/personality-test");
+
+      setPersonalityTests(response.data.personalityTests);
+    } catch (err) {
+      console.error(err);
+    }
+  };
+
+  useEffect(() => {
+    fetchPersonalityTests();
+  }, []);
+
   return (
-    <div>
-      <PersonalityTest /> 
+    <div className="max-w-7xl mx-auto px-4">
+      <div className="grid grid-cols-1 md:grid-cols-2 gap-6 mb-30">
+        {personalityTests.length === 0
+          ? "Wala pang personality test"
+          : personalityTests.map((test) => (
+              <TestCardComponent
+                key={test.testId}
+                title={test.testTitle}
+                description={test.testDescription}
+                duration={test.duration}
+                text="Go to Test"
+              />
+            ))}
+      </div>
     </div>
   );
 };
