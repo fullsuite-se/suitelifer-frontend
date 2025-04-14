b10741bffdab376ff6a04529c6f0544123d88e9c
AllenJalvaro <alllenjjames@gmail.com>
2025-04-14 17:39:37 +0800

    feat: implement FAQ fetching and management with API integration

--- src/components/admin/FAQs.jsx
+++ src/components/admin/FAQs.jsx
@@ -1,6 +1,6 @@
 "use client";
 
-import React, { useState, useRef } from "react";
+import React, { useState, useRef, useEffect } from "react";
 import {
   Dialog,
   DialogTitle,
@@ -17,36 +17,43 @@ import "@ag-grid-community/styles/ag-grid.css";
 import "@ag-grid-community/styles/ag-theme-quartz.css";
 import ContentButtons from "./ContentButtons";
 import { PlusCircleIcon } from "@heroicons/react/24/outline";
+import api from "../../utils/axios";
+
+import { useStore } from "../../store/authStore";
+import toast from "react-hot-toast";
 
 ModuleRegistry.registerModules([ClientSideRowModelModule]);
 
 function FAQs() {
-  const [faqs, setFaqs] = useState([
-    {
-      faqId: "1",
-      question: "What is your return policy?",
-      answer: "30 days return policy.",
-      createdAt: new Date().toISOString(),
-      createdBy: "Melbraei Santiago",
-    },
-    {
-      faqId: "2",
-      question: "Do you offer customer support?",
-      answer: "Yes, 24/7 support available.",
-      createdAt: new Date().toISOString(),
-      createdBy: "Melbraei Santiago",
-    },
-    {
-      faqId: "3",
-      question: "Benz likes Maricar?",
-      answer: "Yes, He does.",
-      createdAt: new Date().toISOString(),
-      createdBy: "Benz",
-    },
-  ]);
+
+
+    const user = useStore((state) => state.user);
+
+  const [faqs, setFaqs] = useState([]);
+
+  const fetchFaqs = async () => {
+    try {
+      const response = await api.get("/api/get-all-faqs");
+      const fetchedFaqs = response.data.faqs;
+      setFaqs(  fetchedFaqs);
+      console.log(fetchedFaqs);
+    } catch (err) {
+      console.log(err);
+    }
+  };
+
+
+  const [dataUpdated, setDataUpdated] = useState(false);
+
+  useEffect(() => {
+    fetchFaqs();
+  }, [dataUpdated]);
+
+
+
   const [openDialog, setOpenDialog] = useState(false);
   const [currentFAQ, setCurrentFAQ] = useState({
-    faqId: "",
+    faq_id: "",
     question: "",
     answer: "",
     createdAt: "",
@@ -55,22 +62,43 @@ function FAQs() {
 
   const gridRef = useRef();
 
-  const handleSave = () => {
-    if (currentFAQ.faqId) {
+  const handleSave = async ()=> {
+    if (currentFAQ.faq_id) {
       setFaqs((prev) =>
-        prev.map((faq) => (faq.faqId === currentFAQ.faqId ? currentFAQ : faq))
+        prev.map((faq) => (faq.faq_id === currentFAQ.faq_id ? currentFAQ : faq))
       );
+
     } else {
       const newFaq = {
         ...currentFAQ,
-        faqId: Date.now().toString(),
-        createdAt: new Date().toISOString(),
-        createdBy: "Melbraei Santiago",
+        created_at: new Date().toISOString(),
+        user_id: user.id
       };
-      setFaqs((prev) => [...prev, newFaq]);
+      setFaqs((prev) => [newFaq, ...prev]);
+      try {
+        console.log("Sending to backend:", newFaq);
+
+        const response = await api.post("/api/add-faq", newFaq);
+        console.log(response.data);
+
+        if (response.data?.success) {
+          toast.success(response.data.message);
+        } else {
+          toast.error(response.data.message || "Failed to save faq.");
+        }
+  
+        setDataUpdated(!dataUpdated);
+
+
+
+      } catch (err) {
+        console.error(err.message);
+      }
+
+
     }
 
-    setCurrentFAQ({ faqId: "", question: "", answer: "" });
+    setCurrentFAQ({ faq_id: "", question: "", answer: "" });
     setOpenDialog(false);
   };
 
@@ -79,19 +107,29 @@ function FAQs() {
     setOpenDialog(true);
   };
 
-  const handleDelete = (faqId) => {
-    setFaqs((prev) => prev.filter((faq) => faq.faqId !== faqId));
+  const handleDelete = (faq_id) => {
+    setFaqs((prev) => prev.filter((faq) => faq.faq_id !== faq_id));
+  };
+
+  const handleFaqDetailsChange = (e) => {
+    setCurrentFAQ((td) => ({ ...td, [e.target.name]: e.target.value }));
+    console.log(currentFAQ);
+    // console.log(imageFile);
   };
 
   return (
     <>
-      <div className="flex justify-end gap-2 mb-2">
-        <ContentButtons
-          icon={<PlusCircleIcon className="size-5" />}
-          text="Add FAQ"
-          handleClick={setOpenDialog}
-        />
-      </div>
+<div className="flex justify-end gap-2 mb-2">
+  <ContentButtons
+    icon={<PlusCircleIcon className="size-5" />}
+    text="Add FAQ"
+    handleClick={() => {
+      setOpenDialog(true);
+      setCurrentFAQ({ faq_id: "", question: "", answer: "" });
+    }}
+  />
+</div>
+
 
       <div
         className="ag-theme-quartz min-w-[600px] lg:w-full "
@@ -124,15 +162,22 @@ function FAQs() {
                 whiteSpace: "normal",
               },
             },
-
+            {
+              headerName: "Visibility",
+              field: "is_shown",
+              flex: 1,
+              headerClass: "text-primary font-bold bg-gray-100",
+              valueFormatter: (params) =>
+                params.value === 1 ? "Shown" : "Hidden",
+            },
             {
               headerName: "Date Created",
               field: "createdAt",
               flex: 2,
               headerClass: "text-primary font-bold bg-gray-100",
               valueGetter: (params) =>
-                params.data?.createdAt
-                  ? new Date(params.data.createdAt).toLocaleString()
+                params.data?.created_at
+                  ? new Date(params.data.created_at).toLocaleString()
                   : "N/A",
             },
             {
@@ -151,7 +196,7 @@ function FAQs() {
                   <IconButton onClick={() => handleEdit(params.data)}>
                     <EditIcon />
                   </IconButton>
-                  <IconButton onClick={() => handleDelete(params.data.faqId)}>
+                  <IconButton onClick={() => handleDelete(params.data.faq_id)}>
                     <DeleteIcon />
                   </IconButton>
                 </div>
@@ -174,8 +219,8 @@ function FAQs() {
         />
       </div>
 
-      <Dialog open={openDialog} onClose={() => setOpenDialog(false)} fullWidth>
-        <DialogTitle>{currentFAQ.faqId ? "Edit FAQ" : "Add FAQ"}</DialogTitle>
+      <Dialog open={openDialog} onClose={() => setOpenDialog(false)} fullWidth > 
+        <DialogTitle>{currentFAQ.faq_id ? "Edit FAQ" : "Add FAQ"}</DialogTitle>
         <DialogContent>
           <div className="mb-4">
             <label className="block text-gray-700 font-bold">Question</label>
@@ -200,6 +245,28 @@ function FAQs() {
               placeholder="Enter the answer"
             />
           </div>
+          <div>
+                <label className="block text-gray-700 font-avenir-black">
+                  Visibility<span className="text-primary">*</span>
+                </label>
+                <select
+                  name="is_shown"
+                  required
+                  value={
+                    currentFAQ.is_shown !== undefined
+                      ? currentFAQ.is_shown
+                      : ""
+                  }
+                  onChange={(e) => handleFaqDetailsChange(e)}
+                  className="w-full p-3 border-none rounded-md bg-primary/10 focus:outline-none focus:ring-2 focus:ring-primary mt-2"
+                >
+                  <option value="" disabled>
+                    -- Select an option --
+                  </option>
+                  <option value={1}>Shown</option>
+                  <option value={0}>Hidden</option>
+                </select>
+              </div>
         </DialogContent>
         <DialogActions>
           <button className="btn-light" onClick={() => setOpenDialog(false)}>
