7efa9598e1373add1da80a27780916e294ae5c7b
ljsalcedo-dev <jau.salcedo04@gmail.com>
2025-04-14 14:54:40 +0800

    fix: update error handling and improve testimonial fetching logic

--- src/components/about-us/TestimonialSection.jsx
+++ src/components/about-us/TestimonialSection.jsx
@@ -8,31 +8,32 @@ import { useEffect, useState } from "react";
 import Skeleton from "react-loading-skeleton";
 
 const Testimonials = () => {
-
   const [testimonials, setTestimonials] = useState([]);
   const [isLoading, setIsLoading] = useState(true);
 
+  const fetchTestimonials = async () => {
+    try {
+      const response = await api.get("/api/testimonials");
+      const data = response.data.testimonials;
+      const filteredTestimonials = data.filter(
+        (testimonial) => !!testimonial.isShown
+      );
+
+      console.log(response.data.testimonials);
+
+      setTestimonials(filteredTestimonials);
+    } catch (error) {
+      console.error(error);
+    } finally {
+      setIsLoading(false);
+      console.log(testimonials.length);
+      
+    }
+  };
+
   useEffect(() => {
-    const fetchTestimonials = async () => {
-      try {
-        const response = await api.get("/api/get-all-testimonials");
-        const data = response.data?.testimonials || [];
-        const filteredTestimonials = data.filter(testimonial => !!testimonial.is_shown);
-       
-        
-        setTestimonials(filteredTestimonials);
-      } catch (error) {
-        console.error(error);
-      } finally {
-        setIsLoading(false);
-      }
-    };
-  
     fetchTestimonials();
   }, []);
-  
-
-
 
   return (
     <section className="py-15">
@@ -69,86 +70,97 @@ const Testimonials = () => {
           }}
           className="w-full"
         >
-   {isLoading
-      ? 
-        Array(5)
-          .fill(null)
-          .map((_, index) => (
-            <SwiperSlide key={index}>
-            {({ isActive }) => (
-              <div
-                className={`p-10 mt-4 ease-out bg-white shadow-lg rounded-lg text-center transition-transform duration-300 ${
-                  isActive
-                    ? "scale-100 md:scale-105 bg-primary text-white"
-                    : "scale-90 opacity-75 hover:scale-95 hover:-translate-y-2"
-                }`}
-              >
-               
-                <div className="size-30 mx-auto rounded-full mb-4 overflow-hidden">
-                  <Skeleton circle width={120} height={120} />
-                </div>
-  
-                <Skeleton width={"80%"} height={16} className="mx-auto mb-2" />
-                <Skeleton width={"90%"} height={16} className="mx-auto mb-2" />
-                <Skeleton width={"70%"} height={16} className="mx-auto mb-4" />
-  
-                <Skeleton width={"40%"} height={18} className="mx-auto mb-1" />
-  
-                <Skeleton width={"30%"} height={14} className="mx-auto text-gray-400" />
-              </div>
-            )}
-          </SwiperSlide>
-          ))
-      : testimonials.length > 0 ? (
-        testimonials.map((testimonial) => (
-          <SwiperSlide key={testimonial.testimonial_id}>
-            {({ isActive }) => (
-              <div
-                className={`p-10 mt-4 ease-out bg-white shadow-lg rounded-lg text-center transition-transform duration-300 ${
-                  isActive
-                    ? "scale-100 md:scale-105 bg-primary text-white"
-                    : "scale-90 opacity-75 hover:scale-95 hover:-translate-y-2"
-                }`}
-              >
-                <img
-                  src={bgQuotes}
-                  alt="quote"
-                  className="absolute translate-x-5 translate-y-22 -rotate-5 w-16 mb-4"
-                />
-                <img
-                  src={testimonial.employee_image_url}
-                  alt={testimonial.employee_name}
-                  className="size-30 mx-auto rounded-full mb-4 object-cover"
-                />
-                <p className="mt-4 text-gray-700 text-body">
-                  {testimonial.testimony}
-                </p>
-                <br />
-                <p className="text-small font-avenir-black text-primary">
-                  {testimonial.employee_name}
-                </p>
-                <p className="text-xss text-gray-400">{testimonial.position}</p>
-              </div>
-            )}
-          </SwiperSlide>
-        )
-      )
-        
-         
-        )
-      
-      : (
-        <div className="text-center p-5 bg-white shadow-lg rounded-lg">
-          <p className="text-body text-gray-600">
-            No Testimonials Yet
-          </p>
-          <p className="text-gray-500 mt-2 text-small">
-          Stay tuned! Employee testimonials will be available soon.
-          </p>
-        </div>
-      )
-      }
+          {isLoading ? (
+            Array(5)
+              .fill(null)
+              .map((_, index) => (
+                <SwiperSlide key={index}>
+                  {({ isActive }) => (
+                    <div
+                      className={`p-10 mt-4 ease-out bg-white shadow-lg rounded-lg text-center transition-transform duration-300 ${
+                        isActive
+                          ? "scale-100 md:scale-105 bg-primary text-white"
+                          : "scale-90 opacity-75 hover:scale-95 hover:-translate-y-2"
+                      }`}
+                    >
+                      <div className="size-30 mx-auto rounded-full mb-4 overflow-hidden">
+                        <Skeleton circle width={120} height={120} />
+                      </div>
 
+                      <Skeleton
+                        width={"80%"}
+                        height={16}
+                        className="mx-auto mb-2"
+                      />
+                      <Skeleton
+                        width={"90%"}
+                        height={16}
+                        className="mx-auto mb-2"
+                      />
+                      <Skeleton
+                        width={"70%"}
+                        height={16}
+                        className="mx-auto mb-4"
+                      />
+
+                      <Skeleton
+                        width={"40%"}
+                        height={18}
+                        className="mx-auto mb-1"
+                      />
+
+                      <Skeleton
+                        width={"30%"}
+                        height={14}
+                        className="mx-auto text-gray-400"
+                      />
+                    </div>
+                  )}
+                </SwiperSlide>
+              ))
+          ) : testimonials.length > 0 ? (
+            testimonials.map((testimonial) => (
+              <SwiperSlide key={testimonial.testimonialId}>
+                {({ isActive }) => (
+                  <div
+                    className={`p-10 mt-4 ease-out bg-white shadow-lg rounded-lg text-center transition-transform duration-300 ${
+                      isActive
+                        ? "scale-100 md:scale-105 bg-primary text-white"
+                        : "scale-90 opacity-75 hover:scale-95 hover:-translate-y-2"
+                    }`}
+                  >
+                    <img
+                      src={bgQuotes}
+                      alt="quote"
+                      className="absolute translate-x-5 translate-y-22 -rotate-5 w-16 mb-4"
+                    />
+                    <img
+                      src={testimonial.employeeImageUrl}
+                      alt={testimonial.employeeName}
+                      className="size-30 mx-auto rounded-full mb-4 object-cover"
+                    />
+                    <p className="mt-4 text-gray-700 text-body">
+                      {testimonial.testimony}
+                    </p>
+                    <br />
+                    <p className="text-small font-avenir-black text-primary">
+                      {testimonial.employeeName}
+                    </p>
+                    <p className="text-xss text-gray-400">
+                      {testimonial.position}
+                    </p>
+                  </div>
+                )}
+              </SwiperSlide>
+            ))
+          ) : (
+            <div className="text-center p-5 bg-white shadow-lg rounded-lg">
+              <p className="text-body text-gray-600">No Testimonials Yet</p>
+              <p className="text-gray-500 mt-2 text-small">
+                Stay tuned! Employee testimonials will be available soon.
+              </p>
+            </div>
+          )}
         </Swiper>
       </div>
     </section>
