20a1f3adadc294394084c04dbc34f43dd9b36e0c
domingohernani <dev.hernani.domingo@gmail.com>
2025-04-17 10:47:59 +0800

    fix: update title formatting in CMSTopNavigation for specific routes

--- src/components/navigation/CMSTopNavigation.jsx
+++ src/components/navigation/CMSTopNavigation.jsx
@@ -8,9 +8,12 @@ const CMSTopNavigation = () => {
   const path = location.pathname.split("/");
   const currentPage = path[path.length - 1];
 
-  console.log(path);
-
   const formatTitle = (str) => {
+    if (path.includes("my-blogs")) {
+      return "My Blog";
+    } else if (path.includes("blogs-feed")) {
+      return "Blogs Feed";
+    }
     return str
       .replace(/-/g, " ")
       .replace(/\b\w/g, (char) => char.toUpperCase());
@@ -18,11 +21,9 @@ const CMSTopNavigation = () => {
 
   return (
     <nav className="p-2 xl:pt-3">
-      <div className="lg:flex items-center justify-between hidden">
+      <div className="lg:flex items-center justify-between">
         <div className="flex items-center gap-2">
-          <h2 className="font-avenir-black truncate w-48">
-            {formatTitle(currentPage)}
-          </h2>
+          <h2 className="font-avenir-black">{formatTitle(currentPage)}</h2>
           <span className="text-sm text-gray-500 font-avenir-roman">(6)</span>
           <InformationCircleIcon className="w-4 h-4 text-gray-500" />
         </div>
