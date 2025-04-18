16fe7dc4ebe521501d063cc8a2b30866aed3d41a
dan-galano <mooncoreee25@gmail.com>
2025-04-18 15:50:45 +0800

    feat: refactor ImageUploader to use forwardRef for improved ref handling

--- src/components/admin/ImageUploader.jsx
+++ src/components/admin/ImageUploader.jsx
@@ -1,7 +1,7 @@
-import React from "react";
+import React, { forwardRef } from "react";
 import { PlusCircleIcon } from "@heroicons/react/24/outline";
 
-const ImageUploader = ({ name, image, onImageChange }) => {
+const ImageUploader = forwardRef(({ name, image, onImageChange }, ref) => {
   return (
     <label
       htmlFor={`${name}-upload`}
@@ -9,6 +9,7 @@ const ImageUploader = ({ name, image, onImageChange }) => {
     >
       <input
         type="file"
+        ref={ref}
         id={`${name}-upload`}
         accept=".jpeg,.jpg,.png,.heic"
         className="hidden"
@@ -24,6 +25,6 @@ const ImageUploader = ({ name, image, onImageChange }) => {
       <p className="text-xss -mt-4 text-gray-500">Max: 10MB</p>
     </label>
   );
-};
+});
 
 export default ImageUploader;
