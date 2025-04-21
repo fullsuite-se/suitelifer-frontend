95ef373f70d9210ff32748b8158406798867ca8f
MB-Tiago <melbraeisantiago@gmail.com>
2025-04-21 19:19:20 +0800

    feat: add onFilterChange prop to ButtonsSpotify for improved button click handling

--- src/components/admin/ButtonsSpotify.jsx
+++ src/components/admin/ButtonsSpotify.jsx
@@ -1,6 +1,6 @@
 import { useState } from "react";
 
-const ButtonsSpotify = ({ buttons }) => {
+const ButtonsSpotify = ({ buttons, onFilterChange }) => {
   const [activeTab, setActiveTab] = useState(0);
 
   const buttonBase =
@@ -8,13 +8,18 @@ const ButtonsSpotify = ({ buttons }) => {
   const activeStyles = "bg-primary/10 text-primary font-avenir-black";
   const inactiveStyles = "border border-gray-300 bg-white";
 
+  const handleClick = (index, label) => {
+    setActiveTab(index);
+    onFilterChange(label);
+  };
+
   return (
     <div className="max-w-[1200px] mx-auto p-4">
-      <div className="flex flex-col sm:flex-row gap-4 ">
+      <div className="flex flex-col sm:flex-row gap-4">
         {buttons.map((button, index) => (
           <button
             key={button.label}
-            onClick={() => setActiveTab(index)}
+            onClick={() => handleClick(index, button.label)}
             className={`${buttonBase} ${
               activeTab === index ? activeStyles : inactiveStyles
             }`}
