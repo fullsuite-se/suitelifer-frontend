0c003cb1bd7d5eb6f171d206d9996ddfa76b492c
dan-galano <mooncoreee25@gmail.com>
2025-04-22 10:52:26 +0800

    feat: implement Coming Soon display in EmployeeThreads component

--- src/pages/employee/EmployeeThreads.jsx
+++ src/pages/employee/EmployeeThreads.jsx
@@ -1,6 +1,14 @@
 import React from "react";
+import { useState } from "react";
+import ComingSoon from "../admin/ComingSoon";
 
 const EmployeeThreads = () => {
+  const [isComingSoon, setComingSoon] = useState(true); //Change this when the page is ready.
+
+  if (isComingSoon) {
+    return <ComingSoon />;
+  }
+
   return <div>EmployeeThreads</div>;
 };
 
