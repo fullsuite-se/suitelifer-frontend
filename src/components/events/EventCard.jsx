7fbc5bc8933a46e6afa60c41401f80d94400d5e9
ljsalcedo-dev <jau.salcedo04@gmail.com>
2025-04-24 15:48:22 +0800

    fix: update EventCard to use event.start for date formatting

--- src/components/events/EventCard.jsx
+++ src/components/events/EventCard.jsx
@@ -2,20 +2,18 @@ import React from "react";
 import { ListBulletIcon, ClockIcon } from "@heroicons/react/24/outline";
 import { format } from "date-fns";
 
-const EventCard = ({ event, isUpcoming }) => {
-  const dayAbbreviation = format(new Date(event.dateStart), "EEE");
-  const dayOfMonth = format(new Date(event.dateStart), "d");
+const EventCard = ({ event }) => {
+  const dayAbbreviation = format(new Date(event.start), "EEE");
+  const dayOfMonth = format(new Date(event.start), "d");
 
   return (
     <section
-      className={`border border-gray-200 p-4 rounded-lg flex gap-4 items-center
-      ${isUpcoming ? "flex gap-4 items-center" : ""}  
-      `}
+      className={
+        "border border-gray-200 p-4 rounded-lg flex gap-4 items-center"
+      }
     >
       <div
-        className={`flex-col items-center justify-center border-r pr-3 border-gray-300
-        ${isUpcoming ? "flex" : "hidden"}  
-          `}
+        className={`flex-col items-center justify-center border-r pr-3 border-gray-300 flex`}
       >
         <span className="text-black text-base">{dayAbbreviation}</span>
         <h1 className="font-avenir-black my-0! text-primary">{dayOfMonth}</h1>
@@ -33,7 +31,7 @@ const EventCard = ({ event, isUpcoming }) => {
         <div className="flex items-start gap-3">
           <ClockIcon className="size-5 text-primary flex-shrink-0" />
           <p className="line-clamp-2 text-sm overflow-hidden font-avenir-black text-gray-400">
-            {format(event.dateStart, "MMM dd yyyy h:mm a")}
+            {format(event.start, "MMM dd yyyy h:mm a")}
           </p>
         </div>
       </div>
