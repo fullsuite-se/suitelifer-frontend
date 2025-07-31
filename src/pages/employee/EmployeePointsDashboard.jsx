7a786db02c7514f00194a0581dc03a935865f536
Francis Nieves <hed-fjnieves@smu.edu.ph>
2025-07-31 15:56:04 +0800

    style: update background color in EmployeePointsDashboard for improved aesthetics

--- src/pages/employee/EmployeePointsDashboard.jsx
+++ src/pages/employee/EmployeePointsDashboard.jsx
@@ -240,7 +240,7 @@ const PointsDashboard = () => {
   };
 
   return (
-    <div className="min-h-screen bg-gray-50 py-8">
+    <div className="min-h-screen bg-white py-8">
       {/* Moderation Notification Banner */}
       {moderationNotification && (
         <div className="max-w-7xl mx-auto px-4 sm:px-6 lg:px-8 mb-3">
